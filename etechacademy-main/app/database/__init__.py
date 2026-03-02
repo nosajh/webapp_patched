ca47874c63fe44209ecb7330e84f5aebb66bc1f4
nosajh <huangjason226@gmail.com>
2026-03-01 20:03:31 -0800

    Delete etechacademy-main directory

--- etechacademy-main/app/database/__init__.py
+++
@@ -1,2 +0,0 @@
-from .connection import get_db_connection
-from .setup import create_tables
--- etechacademy-main/app/routes/endpoint.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-endpoint_bp = Blueprint("endpoint", __name__)
-
-@endpoint_bp.route("/endpoint", methods=['POST'])
-def endpoint():
-
-    return jsonify({'status': 'Function disabled for security'}), 200
